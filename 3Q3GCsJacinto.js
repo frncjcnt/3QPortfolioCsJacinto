dae4d42321d144f97be9aa052aa20d032eca4516
frncjcnt <b2029fmajacinto@pshs.edu.ph>
2026-02-05 22:52:53 +0800

    debugging was like 4h writing was like 1h total

--- 3Q3GCsJacinto.js
+++ 3Q3GCsJacinto.js
@@ -2,13 +2,22 @@ let acctString = localStorage.getItem("accounts")
 if (!acctString) { accountList = {} }
 else accountList = JSON.parse(acctString)
 
-const form = document.getElementById("sForm");
+const form = document.getElementById("s");
+const storageKey = "clubSignUps";
 
-form.addEventListener("submit", function(e){
-if(!confirm("Are you sure all details in the submission are complete and true?"))
+function confirmSubmit() {
+    return confirm("Are you sure you want to submit this application?");
+}
+
+function confirmReset() {
+    return confirm("Are you sure you want to reset the form?");
+}
+
+/*form.addEventListener("submit", function(e){
+if(!confirm(confirmSubmit()))
 e.preventDefault();
 });
-
+*/
 function checkBlank(field){
     if(field.value.trim() === ""){
         field.classList.add("Error");
@@ -18,17 +27,52 @@ function checkBlank(field){
 form.addEventListener("blur", function(e){
     checkBlank(this);
 })
-
+/*
 form.addEventListener("reset", function(e) {
-    if (!confirm("Sure you want to clear your data?")) {
+    if (!confirm(confirmReset())) {
       e.preventDefault(); 
     }
   });
-  
+  */
   function changeColor(ele) {
     console.log(ele);
     ele.style.backgroundColor = "blue";
   } 
 
-const form = document.getElementById("s");s
+function saveSignup(event){
+  event.preventDefault();
+
+  if(!confirmSubmit()){
+    return;
+  }
+
+  const signUp = {
+    studentID: form.studentID.value,
+    userName: form.userName.value,
+    birthDay: form.birthDay.value,
+    eMail: form.eMail.value,
+    mobilePhone: form.mobilePhone.value,
+    gradeLevel: form.gradeLevel.value,
+    residencyType: form.residencyType.value,
+    preferredEC: form.preferredEC.value,
+    club: form.preferredEC.value,
+    reason: form.reason.value
+  };
+
+  let signups = localStorage.getItem(storageKey);
+
+    if (signups) {signups = JSON.parse(signups);}
+    else{signups = [];}
+   
+    signups.push(signUp);
+ 
+    localStorage.setItem(storageKey, JSON.stringify(signups));
+    alert("Successful sign-up!");
+    form.reset();
+}
+
+function goToView() {
+    window.location.href = "viewSignUps.html";
+}
+
 
